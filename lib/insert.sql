f10e50e0087f4c148c1668becab48bd72c5f046a
BarakaIsaac <isaac.mwandau2@gmail.com>
2023-04-10 11:56:04 +0300

    Completed Assignments

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+insert into bears (name, age, sex, color, temperament,alive) values ("Mr. Chocolate", 7, "F", "brown","grumpy",1),
+("Rowdy", 8, "F", "black","energetic",0),
+("Tabitha", 10, "F", "brown","friendly",0),
+("Sergeant Brown", 11, "M", "brown","strict",0),
+("Melissa", 2, "F", "white","shy",1),
+("Grinch", 1, "M", "green","sour",1),
+("Wendy", 12, "F", "green","happy",1),
+(null, 1, "F", "blue","unkown",1);
